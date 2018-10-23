67597527c124a666cdb435cd8bc2696428bccdfc
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-23 14:24:02 -0700

    Create controllers

--- Rails practice assessment/skeleton/config/routes.rb
+++ Rails practice assessment/skeleton/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
 
-  resources :links
-  resources :comments
+  resources :links do
+    resources :comments, only: [:create]
+  end
+
+  resources :comments, only: [:destroy]
 end
