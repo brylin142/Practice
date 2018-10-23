5689ef846e67b128dff2205f4414ef1049802011
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-23 13:31:52 -0700

    Create users and sessions controllers

---
+++ Rails practice assessment/skeleton/app/controllers/links_controller.rb
@@ -0,0 +1,19 @@
+class LinksController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def link_params
+  end
+end
