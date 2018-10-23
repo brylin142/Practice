6f7d298cd0d167048c601390bf3fb95d9bc9c65c
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-23 12:51:09 -0700

    Create links and comments models

--- Rails practice assessment/skeleton/db/schema.rb
+++ Rails practice assessment/skeleton/db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181023185245) do
+ActiveRecord::Schema.define(version: 20181023191952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.integer "user_id", null: false
+    t.integer "link_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_comments_on_link_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
