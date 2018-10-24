11aa4eb5a5f48ce77fae449196da6e6b9075ad2c
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-24 15:05:41 -0700

    Deleted React practice assessment. Wrong folder

--- React practice assessment/skeleton/frontend/reducers/root_reducer.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import PostsReducer from './posts_reducer';
-
-export default combineReducers({posts: PostsReducer});
--- React practice assessment/solution/frontend/reducers/root_reducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import PostsReducer from './posts_reducer';
-
-export default combineReducers({
-  posts: PostsReducer
-});
