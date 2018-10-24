2090eccd312a487f6270163ada2deab2ee74632e
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-24 16:45:33 -0700

    Create reducers and store

--- React practice assessment/skeleton/frontend/reducers/posts_reducer.js
+++ React practice assessment/skeleton/frontend/reducers/posts_reducer.js
@@ -5,8 +5,20 @@ import {
 } from '../actions/post_actions';
 import merge from 'lodash/merge';
 
-const PostsReducer = (oldState, action) => {
-
+const PostsReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch(action.type) {
+    case RECEIVE_ALL_POSTS:
+      return merge({}, oldState, action.posts);
+    case RECEIVE_POST:
+      return merge({}, oldState, { [action.post.id]: action.post });
+    case REMOVE_POST:
+      let newState = merge({}, oldState);
+      delete newState[action.postId];
+      return newState;
+    default:
+      return oldState;
+  }
 };
 
 export default PostsReducer;
--- React practice assessment/skeleton/frontend/store/store.js
+++ React practice assessment/skeleton/frontend/store/store.js
@@ -3,7 +3,8 @@ import RootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 
 const configureStore = (preloadedState = {}) => {
-
+  const store = createStore(RootReducer, preloadedState, applyMiddleware(thunk));
+  return store;
 };
 
 export default configureStore;
