2aacddd9e99e9d64c2d17c79dde2fe8fd3851f72
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-26 14:15:11 -0700

    Create post show components

--- React practice assessment/skeleton/frontend/components/posts/post_show.jsx
+++ React practice assessment/skeleton/frontend/components/posts/post_show.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class PostShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchPost(this.props.match.params.postId);
+  }
 
   render () {
     return (
       <div>
+        <p>{ this.props.post.title }</p>
+        <p>{ this.props.post.body }</p>
+        <Link to='/'>Post Index</Link>
       </div>
     );
   }
