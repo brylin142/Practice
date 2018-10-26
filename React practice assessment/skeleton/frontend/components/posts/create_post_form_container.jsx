505341b5f04a9d6b6607346d610a37e4de9a5755
brylin142 <33795878+brylin142@users.noreply.github.com>
2018-10-26 13:45:31 -0700

    Make create post form and post form

--- React practice assessment/skeleton/frontend/components/posts/create_post_form_container.jsx
+++ React practice assessment/skeleton/frontend/components/posts/create_post_form_container.jsx
@@ -2,3 +2,13 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import PostForm from './post_form';
 import { createPost } from '../../actions/post_actions';
+
+const msp = state => ({
+  post: { title: '', body: '' }
+});
+
+const mdp = dispatch => ({
+  action: post => dispatch(createPost(post))
+});
+
+export default connect(msp, mdp)(PostForm);
--- React practice assessment/skeleton/frontend/components/posts/post_form.jsx
+++ React practice assessment/skeleton/frontend/components/posts/post_form.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class PostForm extends React.Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: ''
+    };
+  }
+
+  updateTitle(e) {
+    this.setState({ title: e.target.value });
+  }
+  
+  updateBody(e) {
+    this.setState({ body: e.target.value });
+  }
+
   render () {
     return (
       <div>
+        <form onSubmit={ () => this.props.action(this.state) }>
+          <input onChange={ this.updateTitle.bind(this) } type='text' value={ this.state.title }></input>
+          <textarea onChange= { this.updateBody.bind(this) } value={ this.state.body }>Body</textarea>
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     );
   }
